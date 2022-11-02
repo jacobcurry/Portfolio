bd1003786b8943158a037aec03ca322c8d7f1735
Jacob Curry <jacob.a.curry@gmail.com>
2022-11-02 15:09:10 -0400

    finsishd js carassuel

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//change the word hello to mulitple langauges
 const helloArr = ["Hello", "Hola", "Bonjour", "Hello"];
 let changeHelloNum = 0;
 let stringNum = 0;
@@ -22,11 +23,57 @@ const changeHello = () => {
 };
 const intervalId = window.setInterval(changeHello, 400);
 
-const headerDropDown = () => {
-  $(".home-html").slideDown("slow");
-  $(".bio-html").slideDown("slow");
-  $(".resume-html").slideDown("slow");
-  $(".projects-html").slideDown("slow");
+//changes teh color of the nav bar link that is currently active
+const currentNavColor = () => {
+  for (let i = 0; i < $(".nav").length; i++) {
+    if (document.URL.includes($(".nav").eq(i).attr("href"))) {
+      $(".nav").eq(i).addClass("current");
+    }
+  }
 };
 
-$(() => {});
+//carousell (if that is how you spell it) for about me page.
+let currentDiv = 1;
+const nextButton = () => {
+  let numOfDivs = $(".about-main-container").children().length - 1;
+  $(".about-main-container")
+    .children()
+    .eq(currentDiv)
+    .addClass("hidden")
+    .removeClass("about-text-container");
+  if (currentDiv < numOfDivs) {
+    currentDiv++;
+  } else {
+    currentDiv = 1;
+  }
+  $(".about-main-container")
+    .children()
+    .eq(currentDiv)
+    .addClass("about-text-container")
+    .removeClass("hidden");
+};
+const previousButton = () => {
+  let numOfDivs = $(".about-main-container").children().length - 1;
+  $(".about-main-container")
+    .children()
+    .eq(currentDiv)
+    .addClass("hidden")
+    .removeClass("about-text-container");
+  if (currentDiv > 1) {
+    currentDiv--;
+  } else {
+    currentDiv = numOfDivs;
+  }
+  $(".about-main-container")
+    .children()
+    .eq(currentDiv)
+    .addClass("about-text-container")
+    .removeClass("hidden");
+};
+
+$(() => {
+  currentNavColor();
+
+  $(".next-button").on("click", nextButton);
+  $(".previous-button").on("click", previousButton);
+});
