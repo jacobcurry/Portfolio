59037c2794689c96389479653aaed6631f145eb6
Jacob Curry <jacob.a.curry@gmail.com>
2022-11-03 14:50:10 -0400

    started project page

--- app.js
+++ app.js
@@ -1,29 +1,4 @@
-//change the word hello to mulitple langauges
-const helloArr = ["Hello", "Hola", "Bonjour", "Hello"];
-let changeHelloNum = 0;
-let stringNum = 0;
-let helloString = "";
-const changeHello = () => {
-  let stringArr = helloArr[changeHelloNum].split("");
-  if (stringNum < stringArr.length) {
-    helloString += stringArr[stringNum];
-    $(".hello-text").text(helloString);
-    stringNum++;
-  } else if (changeHelloNum != helloArr.length - 1) {
-    helloString = helloString.slice(0, helloString.length - 1);
-    $(".hello-text").text(helloString);
-  }
-  if (helloString.length === 0 && changeHelloNum < helloArr.length) {
-    changeHelloNum++;
-    stringNum = 0;
-  }
-  if (changeHelloNum === helloArr.length) {
-    return clearInterval(intervalId);
-  }
-};
-const intervalId = window.setInterval(changeHello, 400);
-
-//changes teh color of the nav bar link that is currently active
+//changes the color of the nav bar link that is currently active
 const currentNavColor = () => {
   for (let i = 0; i < $(".nav").length; i++) {
     if (document.URL.includes($(".nav").eq(i).attr("href"))) {
@@ -137,6 +112,28 @@ const skillsButtonClick = () => {
 };
 
 $(() => {
+  let $aboutText = $(".about-textH1");
+  let aboutNum = 0;
+  let aboutString = "Hello, my name is Jacob Curry";
+
+  const printAboutText = setInterval(() => {
+    aboutNum++;
+    $aboutText.text(aboutString.slice(0, aboutNum));
+    if (aboutNum === aboutString.length) {
+      clearInterval(printAboutText);
+      aboutNum = 0;
+      setInterval(() => {
+        if (aboutNum === 0) {
+          $aboutText.text(aboutString + "_");
+          aboutNum = 1;
+        } else {
+          $aboutText.text(aboutString);
+          aboutNum = 0;
+        }
+      }, 500);
+    }
+  }, 150);
+
   //nav bar colors
   currentNavColor();
   //about section buttons
